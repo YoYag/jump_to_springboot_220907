a376fc4d2099bb68c8b7ea05b5994d3d4158336f
YoYag <la2043432@gmail.com>
2022-09-08 12:19:54 +0900

    2-07 질문 목록과 템플릿

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor // final이 붙은 속성을 포함하는 생성자를 자동으로 생성
+@RequestMapping("/question")
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @RequestMapping("/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
