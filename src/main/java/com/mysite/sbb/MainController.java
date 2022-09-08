2c150f6cd6eb47069357a89097d4f56677371000
YoYag <la2043432@gmail.com>
2022-09-08 12:24:46 +0900

    2-08 ROOT URL

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -10,7 +10,16 @@ public class MainController {
     @RequestMapping("/sbb")
     @ResponseBody
     public String index() {
-        System.out.println("index");
-        return "index";
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
     }
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+
+    /*
+    - redirect:<URL> - URL로 리다이렉트 (리다이렉트는 완전히 새로운 URL로 요청이 된다.)
+    - forward:<URL> - URL로 포워드 (포워드는 기존 요청 값들이 유지된 상태로 URL이 전환된다.)
+     */
 }
